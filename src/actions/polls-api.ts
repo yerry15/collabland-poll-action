4f4326abcd1db85c9a57e479e1d57039452d6ba1
Jerry <jf4580@nyu.edu>
2023-05-25 18:43:47 -0700

    changes

--- src/actions/polls-api.ts
+++ src/actions/polls-api.ts
@@ -1,5 +1,36 @@
 import {getFetch, handleFetchResponse, stringify} from '@collabland/common';
-type Vote = {poll_id: string; option_id: string; identifier: string};
+import { DataResolver } from 'discord.js';
+type Vote = {
+  identifier: string;
+  poll_id: string;
+  option_id: string;
+  created_at: string;
+  updated_at: string;
+  id: string;
+  entity: 'Vote';
+};
+type CreateVoteResponse = {
+  status: string;
+  statusCode: number;
+  data: Vote
+}
+type AllVoteAPIresponse = {
+  status: string;
+  statusCode: number;
+  data: {
+    docs: Vote[];
+    totalDocs: number;
+    offset: number;
+    limit: number;
+    totalPages: number;
+    page: number;
+    pagingCounter: number;
+    hasPrevPage: boolean;
+    hasNextPage: boolean;
+    prevPage: null;
+    nextPage: null;
+  };
+};
 
 type Option = {
   data: null | string;
@@ -58,10 +89,10 @@ export class Pollsapi {
         method: 'get',
       },
     );
-    const data = await handleFetchResponse(response);
-    return data;
+    const data = await handleFetchResponse<{data: Poll}>(response);
+    return data.data;
   }
-  async createVote(vote: Vote) {
+  async createVote(vote: Pick<Vote, 'poll_id' | 'option_id' | 'identifier'>) {
     const url = 'https://api.pollsapi.com/v1/create/vote';
 
     const requestOptions = {
@@ -69,11 +100,26 @@ export class Pollsapi {
       body: JSON.stringify(vote),
     };
     const response = await this.fetch(url, requestOptions);
+    const data = await handleFetchResponse<CreateVoteResponse>(response);
+    return data;
+  }
+  async getAllVotesOnPoll(pollId: string, offset = 0, limit = 100) {
+    const url = `https://api.pollsapi.com/v1/get/votes/${pollId}?offset=${offset}&limit=${limit}`;
+    const response = await this.fetch(url, {
+      method: 'get',
+    });
+    const data = await handleFetchResponse<AllVoteAPIresponse>(response);
+    return data;
+  }
+  async removeVote(vote_id:string){
+    const url = 'https://api.pollsapi.com/v1/remove/vote';
+    const response = await this.fetch(url, {
+      method: 'post',
+      body: JSON.stringify({vote_id})
+    })
     const data = await handleFetchResponse(response);
     return data;
-    // Handle the response data here
   }
-  // Handle any errors that occur during the request
 }
 
 async function main() {
@@ -87,6 +133,18 @@ async function main() {
   const pollId = poll.data.id;
   const found = await api.getPoll(pollId);
   console.log(found);
+  const vote = await api.createVote({
+    identifier: 'Jerry',
+    option_id: found.options[0].id,
+    poll_id: pollId,
+  });
+  console.log('This is your vote', vote);
+  const allVotes = await api.getAllVotesOnPoll(pollId);
+  console.log(stringify(allVotes));
+  const votes = allVotes.data.docs.map(d => {return {pollId: d.poll_id, id: d.id, identifier: d.identifier}});
+  console.log(stringify(votes));
+  const deleteVote = await api.removeVote(vote.data.id)
+  console.log(deleteVote);
 }
 
 if (require.main === module) {
